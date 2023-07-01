cdebe4c488adce5ddb85a84574bdecedea23873a
Ruslan Mukhametshin <ruslanow597@gmail.com>
2023-07-01 20:44:57 +0300

    HW2 made. Add list of music genres. Add new class. Refactor methods in MusicPlayer class.

--- src/main/java/ru/mukhametshin/springcourse/MusicPlayer.java
+++ src/main/java/ru/mukhametshin/springcourse/MusicPlayer.java
@@ -1,7 +1,10 @@
 package ru.mukhametshin.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
     private String name;
     private int volume;
 
@@ -22,19 +25,18 @@ public class MusicPlayer {
     }
 
     // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
 
     public MusicPlayer() {
 
     }
 
-    public void setMusic(Music music) {
-        this.music = music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
---
+++ src/main/java/ru/mukhametshin/springcourse/TranceMusic.java
@@ -0,0 +1,8 @@
+package ru.mukhametshin.springcourse;
+
+public class TranceMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Sun and Moon";
+    }
+}
