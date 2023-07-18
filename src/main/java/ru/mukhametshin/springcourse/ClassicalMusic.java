cf81fafb9264f8f367ef568d80b87eac34c6606e
Ruslan Mukhametshin <ruslanow597@gmail.com>
2023-07-18 13:34:32 +0300

    Bean lifecycle.

--- src/main/java/ru/mukhametshin/springcourse/ClassicalMusic.java
+++ src/main/java/ru/mukhametshin/springcourse/ClassicalMusic.java
@@ -1,7 +1,16 @@
 package ru.mukhametshin.springcourse;
 
 public class ClassicalMusic implements Music{
+
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
     public String getSong() {
         return "Hungarian Rhapsody";
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
     }
 }
