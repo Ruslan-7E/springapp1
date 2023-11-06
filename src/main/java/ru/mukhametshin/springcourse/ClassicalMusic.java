9db297f55d518a3a4fd7e79792c028b10d7dc0ae
Ruslan Mukhametshin <ruslanow597@gmail.com>
2023-11-07 02:33:37 +0300

    Initial commit fo HW

--- src/main/java/ru/mukhametshin/springcourse/ClassicalMusic.java
+++ src/main/java/ru/mukhametshin/springcourse/ClassicalMusic.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ClassicalMusic implements Music{
+
     public String getSong() {
         return "Hungarian Rhapsody";
     }
